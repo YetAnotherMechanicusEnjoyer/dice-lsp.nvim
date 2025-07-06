899b1a3e7de3d48a0112f10f106888c6556defa9
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-07-06 04:15:08 +0200

    [UPDATE] init.lua

--- lua/dice_lsp/init.lua
+++ lua/dice_lsp/init.lua
@@ -5,14 +5,16 @@ local M = {}
 function M.setup(opts)
 	opts = opts or {}
 
-	lspconfig.dice_lsp = {
-		default_config = {
-			cmd = opts.cmd or { "dicec" },
-			filetype = { "dice" },
-			root_dir = lspconfig.util.root_pattern(".git", "."),
-			settings = opts.settings or {},
-		},
-	}
+	if not lspconfig.configs.dice_lsp then
+		lspconfig.configs.dice_lsp = {
+			default_config = {
+				cmd = opts.cmd or { "dice-lsp" },
+				filetypes = { "dice" },
+				root_dir = lspconfig.util.root_pattern(".git", "."),
+				settings = opts.settings or {},
+			},
+		}
+	end
 
 	lspconfig.dice_lsp.setup(opts)
 end
