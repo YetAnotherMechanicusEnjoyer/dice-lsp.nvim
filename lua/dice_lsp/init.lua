031d9264a5d99da8e6e7d7f27ef8571a3169d5dc
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-07-06 04:23:58 +0200

    [UPDATE] init.lua

--- lua/dice_lsp/init.lua
+++ lua/dice_lsp/init.lua
@@ -1,12 +1,18 @@
-local lspconfig = require("lspconfig")
+local ok, lspconfig = pcall(require, "lspconfig")
+if not ok then
+	vim.notify("[dice-lsp.nvim] lspconfig not found", vim.log.levels.ERROR)
+	return
+end
+
+local configs = require("lspconfig.configs")
 
 local M = {}
 
 function M.setup(opts)
 	opts = opts or {}
 
-	if not lspconfig.configs.dice_lsp then
-		lspconfig.configs.dice_lsp = {
+	if not configs.dice_lsp then
+		configs.dice_lsp = {
 			default_config = {
 				cmd = opts.cmd or { "dicec" },
 				filetypes = { "dice" },
